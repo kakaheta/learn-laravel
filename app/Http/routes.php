66b5e8cece3a6c788f5e9d6a5f62d0506d31d22c
kakaheta <zym150806@gmail.com>
2016-11-26 22:15:22 +0800

    quickstart tutorial

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +14,46 @@
 |
 */
 
+/**
+ * show task dashboard
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
+
+/**
+ * add a new task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // create the task
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * delete task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
+});
+
